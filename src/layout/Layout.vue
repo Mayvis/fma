d5ade565f816e3652fea2239f8fb23832b389fe7
Mayvis <kevin73911@gmail.com>
2021-11-03 18:52:03 +0800

    adjust footer show

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import AppFooter from "./footer/AppFooter.vue";
 import AppNavbar from "./navbar/AppNavbar.vue";
 
 const key = computed(() => useRoute().path);
+const footerExclude = ref(["/future-vision-lab"]);
 </script>
 
 <template>
-  <app-navbar></app-navbar>
-  <router-view :key="key"></router-view>
-  <app-footer></app-footer>
+  <section class="pt-14">
+    <app-navbar></app-navbar>
+    <router-view :key="key"></router-view>
+    <app-footer v-if="!footerExclude.includes(key)"></app-footer>
+  </section>
 </template>
 
-<style></style>
+<style lang="scss" scoped></style>
